98fd241cc8696cc812d07efe6d1e1ddd5ae09cdc
hayley.j.smith <hayley.j.smith@gmail.com>
2024-01-12 02:45:43 +0000

    deployed

--- lib/start_debate.dart
+++ lib/start_debate.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_const
+
 import 'package:flutter/material.dart';
 
 class StartDebate extends StatefulWidget {
@@ -10,11 +12,28 @@ class StartDebate extends StatefulWidget {
 class _StartDebateState extends State<StartDebate> {
   @override
   Widget build(BuildContext context) {
+    var nameController = TextEditingController();
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(
-        child: Text(
-          "Start debate screen",
+      body: Center(
+        child: Column(
+          children: [
+            //TextField for debate name
+            const Text(
+              "Start debate screen",
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Make an assertion',
+                ),
+                controller: nameController,
+              ),
+            ),
+          ],
         ),
       ),
     );
